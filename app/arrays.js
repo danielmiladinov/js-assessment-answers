2184ff8b34c565556e69f32e98cf7bfacf6c7169
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-05-21 07:18:52 -0400

    Implement arraysAnswers.removeWithoutCopy

--- app/arrays.js
+++ app/arrays.js
@@ -15,7 +15,13 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
