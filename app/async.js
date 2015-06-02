222e5a5c8ead39262487b5005a1e5ee41b24b425
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-02 07:42:27 -0400

    Implement asyncAnswers.manipulateRemoteData

--- app/async.js
+++ app/async.js
@@ -6,6 +6,14 @@ exports.asyncAnswers = {
   },
 
   manipulateRemoteData : function(url) {
-
+    return new Promise(function (resolve, reject) {
+      $.ajax(url).done(function (results) {
+        resolve(results.people.map(function (person) {
+          return person.name;
+        }).sort(function (x, y) {
+          return x > y;
+        }));
+      }).fail(reject);
+    });
   }
 };
