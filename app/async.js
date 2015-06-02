dad51d078f19954a4ffe455a877fdfd2b40489b8
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-01 21:39:01 -0400

    Implement asyncAnswers.resolve

--- app/async.js
+++ app/async.js
@@ -2,7 +2,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return Promise.resolve(value);
   },
 
   manipulateRemoteData : function(url) {
