72c058637c539e5c80aadb541236f02006adcf31
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-04 08:11:09 -0400

    Implement flowControlAnswers.fizzBuzz

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,5 +1,22 @@
 exports = (typeof window === 'undefined') ? global : window;
 
+var fizzBuzzFactory = function () {
+  var divisors = [].slice.apply(arguments);
+
+  return function (num) {
+    function go (n) {
+      var results = divisors.map(function (pair) {
+        var divisor = pair[0], ifDivides = pair[1];
+        return n % divisor === 0 ? ifDivides : '';
+      }).join('').trim();
+
+      return results === '' ? n : results;
+    }
+
+    return $.isNumeric(num) && go(num);
+  };
+};
+
 exports.flowControlAnswers = {
   fizzBuzz : function(num) {
     // write a function that receives a number as its argument;
@@ -10,6 +27,6 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
-
+    return fizzBuzzFactory([3, 'fizz'], [5, 'buzz'])(num);
   }
 };
