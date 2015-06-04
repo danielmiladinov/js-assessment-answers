453f35b41709d6c48df2f214330ad50ad96709d2
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-03 21:56:37 -0400

    Implement countAnswers.count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var canceled = false;
+    var tick = start;
 
+    var doTicks = function () {
+      if (! canceled && tick <= end) {
+        console.log(tick);
+        tick = tick + 1;
+        setTimeout(doTicks, 100);
+      }
+    };
+
+    doTicks();
+
+    return {
+      cancel: function () {
+        canceled = true;
+      }
+    };
   }
 };
