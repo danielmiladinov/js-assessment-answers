19a81e103499649e93d3928f8452ef613529df21
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-14 08:49:02 -0400

    Implement modulesAnswers.createModule

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,12 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
-
+    return {
+      sayIt: function () {
+        return [this.greeting, this.name].join(', ');
+      },
+      name: str2,
+      greeting: str1
+    };
   }
 };
