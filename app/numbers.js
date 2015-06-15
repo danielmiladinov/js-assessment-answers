6a49414b255f24827467edc56b34b8c0957be48f
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-15 09:29:54 -0400

    Implement numbersAnswers.valueAtBit

--- app/numbers.js
+++ app/numbers.js
@@ -2,7 +2,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return 1 & (num >> (bit - 1));
   },
 
   base10: function(str) {
