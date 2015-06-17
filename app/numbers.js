9965bb52cfbc09903178825e6691855537c603ce
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-17 07:55:14 -0400

    Implement numbersAnswers.convertToBinary

--- app/numbers.js
+++ app/numbers.js
@@ -10,7 +10,12 @@ exports.numbersAnswers = {
   },
 
   convertToBinary: function(num) {
-
+    var full = num.toString(2);
+    var asBinary = full.substring(-8);
+    while (asBinary.length < 8) {
+      asBinary = "0" + asBinary;
+    }
+    return asBinary;
   },
 
   multiply: function(a, b) {
