2eaa5f6357b64b8af0697e64af213d5b39fb5df3
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-18 11:56:09 -0400

    Implement numbersAnswers.multiply

--- app/numbers.js
+++ app/numbers.js
@@ -19,6 +19,26 @@ exports.numbersAnswers = {
   },
 
   multiply: function(a, b) {
+    var deDecimalify = function (n) {
+      var numDecimalPlaces = (n.toString().split('.')[1] || []).length,
+          magnitude = Math.pow(10, numDecimalPlaces),
+          deDecimaled = n * magnitude;
 
+      return [deDecimaled, magnitude];
+    };
+
+    var result = [a, b].map(deDecimalify).reduce(function (productPair, factorPair) {
+      var product = productPair[0];
+      var pMagnitude = productPair[1];
+
+      var factor = factorPair[0];
+      var fMagnitude = factorPair[1];
+
+      return [product * factor, pMagnitude * fMagnitude];
+    }, [1, 1]);
+
+    var finalProduct = result[0];
+    var finalMagnitude = result[1];
+    return finalProduct / finalMagnitude;
   }
 };
