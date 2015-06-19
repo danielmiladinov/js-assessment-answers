0f1232297694e3cd9d9d56df7eee7d045d7cb341
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-19 10:27:35 -0400

    Implement objectsAnswers

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.apply(obj, arguments);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+    var own = [];
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        own.push([prop, obj[prop]].join(': '));
+      }
+    }
+    return own;
   }
 };
