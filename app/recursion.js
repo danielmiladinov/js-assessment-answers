4d8a65d495c029dd8da8521628c5d55e1a0e2969
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-20 19:21:57 -0400

    Implement recursionAnswers.listFiles

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var directoryFilter = (dirName === void(0) ?
+      function () { return true; } :
+      function (dn) { return dn === dirName; }
+    );
 
+    var files = [];
+
+    data.files.forEach(function (file) {
+      if (typeof file === "string" && directoryFilter(data.dir)) {
+        files.push(file);
+      } else if (typeof file === "object") {
+        files = files.concat(exports.recursionAnswers.listFiles(file, directoryFilter(data.dir) ? undefined : dirName));
+      }
+    });
+
+    return files;
   },
 
   permute: function(arr) {
