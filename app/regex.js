4a531b2eaec44f0dc6f7632a0df206f8a252bb0d
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-24 10:09:37 -0400

    Implement regexAnswers.containsNumber

--- app/regex.js
+++ app/regex.js
@@ -2,7 +2,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    return /\d/.test(str);
   },
 
   containsRepeatingLetter : function(str) {
