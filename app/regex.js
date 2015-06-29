41f4e801a1e359233a9e48c7437535c67c55f156
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-06-29 06:11:06 -0400

    Implement regexAnswers.isUSD

--- app/regex.js
+++ app/regex.js
@@ -21,7 +21,9 @@ exports.regexAnswers = {
   matchesPattern : function(str) {
     return /^\d{3}-\d{3}-\d{4}$/.test(str);
   },
-  isUSD : function(str) {
 
+  isUSD : function(str) {
+    var isUSD = /^\$\d{1,3},?(\d{3},?)*(\.\d{2})?$/.test(str);
+    return isUSD;
   }
 };
